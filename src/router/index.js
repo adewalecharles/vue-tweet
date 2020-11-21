a50ae17c5be402f840b965fef5ef33a0d9e995ce
adewalecharles <shyprince1@gmail.com>
2020-11-21 18:28:08 +0100

    roue guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import UserProfile from "../views/UserProfile";
+import Admin from "../views/Admin";
 
 const routes = [
   {
@@ -13,6 +14,15 @@ const routes = [
     path:"/user/:userId",
     name: 'UserProfile',
     component: UserProfile
+  },
+
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    meta: {
+      requiresAdmin: true
+    }
   }
   
 ]
@@ -20,6 +30,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to,from,next)=>{
+  const isAdmin = true;
+  const requiresAdmin = to.matched.some(record => record.meta.requiresAdmin);
+
+  if (requiresAdmin && !isAdmin) next({name: 'Home'})
 })
 
 export default router
